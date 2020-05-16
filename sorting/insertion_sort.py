9c9170496b63cdbc3c194fa57238202574ecf970
Muhammadrasul <muhammadrasul446@mail.ru>
2020-05-16 16:57:22 +0500

    Insertion sort

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,12 @@
+# Insertion sort
+
+def insertion_sort(arr):
+	for i in range(0, len(arr)):
+		for j in range(i - 1, -1, -1):
+			if arr[j] > arr[j + 1]:
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+			else:
+				break
+	return arr
+	
+print(insertion_sort([6, 2, 3, 8, 5, 1, 7, 4]))
