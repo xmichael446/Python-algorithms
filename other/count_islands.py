5d19f7418f23c31e0185edc9be68e691ed98743c
Muhammadrasul <64916997+Muhammadrasul446@users.noreply.github.com>
2020-06-13 23:32:31 +0500

    Update count_islands.py

--- other/count_islands.py
+++ other/count_islands.py
@@ -1,58 +1,36 @@
-# An island in matrix is a group of linked areas, all having the same value. 
-# This code couts number of islands in a given matrix, with including diagonal connections.
-
-class matrix: # Public class to implement a graph
- 
-	def __init__(self, row, col, g): # Class constructor
-		self.ROW = row 
-		self.COL = col 
-		self.graph = g
-
-	def is_safe(self, i, j, visited): 
-		return (i >= 0 and i < self.ROW and j >= 0 and j < self.COL and not visited[i][j] and self.graph[i][j]) 
-			 
-	def diffs(self, i, j, visited): # Checking all 8 elements surrounding nth element
- 
-		rowNbr = [-1, -1, -1, 0, 0, 1, 1, 1]; # Coordinate order
-		colNbr = [-1, 0, 1, -1, 1, -1, 0, 1]; 
-		
-		visited[i][j] = True # Make those cells visited
- 
-		for k in range(8): 
-			if self.is_safe(i + rowNbr[k], j + colNbr[k], visited): 
-				self.diffs(i + rowNbr[k], j + colNbr[k], visited) 
- 
- 
-	def count_islands(self): # And finally, count all islandas.
-		visited = [[False for j in range(self.COL)]for i in range(self.ROW)] 
-		count = 0
-		for i in range(self.ROW): 
-			for j in range(self.COL): 
-				if visited[i][j] == False and self.graph[i][j] == 1: 
-					self.diffs(i, j, visited) 
-					count += 1
-		return count 
- 
-# A test matrix
-test_matrix = [
-		[1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0],
-		[0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0],
-		[0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-		[0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1],
-		[0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0],
-		[0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0],
-		[0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
-		[0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-] 
+# An island in matrix is a group of linked areas, all having the same value.
+# This code counts number of islands in a given matrix, with including diagonal
+# connections.
 
 
-row = len(graph)
-col = len(graph[0])
- 
-matrix = matrix(row, col, test_matrix) 
+class matrix:  # Public class to implement a graph
+    def __init__(self, row: int, col: int, graph: list):  # Class constructor
+        self.ROW = row
+        self.COL = col
+        self.graph = graph
 
-print(matrix.count_islands())
+    def is_safe(self, i, j, visited):
+        return (
+            0 <= i < self.ROW
+            and 0 <= j < self.COL
+            and not visited[i][j]
+            and self.graph[i][j]
+        )
+
+    def diffs(self, i, j, visited):  # Checking all 8 elements surrounding nth element
+        rowNbr = [-1, -1, -1, 0, 0, 1, 1, 1]  # Coordinate order
+        colNbr = [-1, 0, 1, -1, 1, -1, 0, 1]
+        visited[i][j] = True  # Make those cells visited
+        for k in range(8):
+            if self.is_safe(i + rowNbr[k], j + colNbr[k], visited):
+                self.diffs(i + rowNbr[k], j + colNbr[k], visited)
+
+    def count_islands(self):  # And finally, count all islands.
+        visited = [[False for j in range(self.COL)] for i in range(self.ROW)]
+        count = 0
+        for i in range(self.ROW):
+            for j in range(self.COL):
+                if visited[i][j] is False and self.graph[i][j] == 1:
+                    self.diffs(i, j, visited)
+                    count += 1
+        return count
