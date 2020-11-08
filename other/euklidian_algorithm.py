80118596ed59f3c2c624a3736f2b22e0a2b5510d
Muhammadrasul <muhammadrasul446@mail.ru>
2020-11-08 09:46:12 +0000

    Optimization

--- other/euklidian_algorithm.py
+++ other/euklidian_algorithm.py
@@ -1,13 +1,13 @@
 # Euklid
 
 def Euklid(a, b):
-	if a == b:
-		return a
-	else:
-		if a > b:
-			return Euklid(b, a - b)
-		else:
-			return Euklid(a, b - a)
+    if a == b:
+        return a
+    else:
+        if a > b:
+            return Euklid(b, a - b)
+        else:
+            return Euklid(a, b - a)
 
 
 print(Euklid(128, 32))
--- other/fibonacci.py
+++ other/fibonacci.py
@@ -1,10 +1,10 @@
 def fibonacci(n):
-	a = b = 1
-	arr = [0]
-	for i in range(n):
-		a, b = b, a + b
-		arr.append(a)
-	return arr
+    a = b = 1
+    arr = [0]
+    for i in range(n):
+        a, b = b, a + b
+        arr.append(a)
+    return arr
 
 
 print(fibonacci(10))
